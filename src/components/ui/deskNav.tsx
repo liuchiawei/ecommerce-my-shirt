985ed8408d35dbb2fd8de6057826b8928d46688d
LIU CHIAWEI <doublecheap@gmail.com>
2025-04-26 18:19:44 +0900

    style - hide desktop nav invisible in mobile layout

--- src/components/ui/deskNav.tsx
+++ src/components/ui/deskNav.tsx
@@ -2,7 +2,7 @@ import { NavigationMenu, NavigationMenuItem, NavigationMenuList, NavigationMenuT
 import NavToolSet from '@/components/NavToolSet'
 export default function DeskNav() {
   return (
-    <div className='flex justify-between items-center w-full h-full'>
+    <div className='hidden md:flex justify-between items-center w-full h-full'>
       <NavigationMenu className='w-full h-full **:cursor-pointer'>
         <NavigationMenuList className='**:data-item:font-bold **:data-item:text-md **:data-item:hover:text-cyan-500 **:data-item:transition-all **:data-item:duration-200 **:data-item:after:content-[attr(data-jp)] **:data-item:after:flex **:data-item:after:items-center **:data-item:after:justify-center **:data-item:after:absolute **:data-item:after:top-0 **:data-item:after:left-0 **:data-item:after:w-full **:data-item:after:h-full **:data-item:after:rounded-sm **:data-item:after:bg-popover **:data-item:after:opacity-0 **:data-item:after:transition-all **:data-item:after:duration-200 **:data-item:after:ease-in-out **:data-item:after:z-10 **:data-item:hover:after:opacity-100'>
           <NavigationMenuItem>
