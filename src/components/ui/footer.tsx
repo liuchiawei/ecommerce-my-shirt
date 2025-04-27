6dbb193d5865901384a55f42d980c2b6c7511563
LIU CHIAWEI <doublecheap@gmail.com>
2025-04-27 15:16:48 +0900

    style - update footer logo

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,16 +1,19 @@
 import SNSLinks from "@/components/SNSLinks";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <div className="grid grid-cols-2 gap-10 w-full h-full py-10 px-6 md:px-10">
       <div className="w-full md:w-[45%]">
-        <div>logo</div>
-        <p className="text-sm">
+        <Link href="/" className="text-4xl">
+          not <span className="font-black"> shirt</span>
+        </Link>
+        <p className="text-sm mt-2">
           ©2025 Liu
           <br /> All rights reserved.
         </p>
       </div>
-      <ul className="w-full flex flex-col md:flex-row justify-between gap-4 px-8 *:flex *:flex-col *:gap-2 **:data-mainlink:font-bold **:data-sublink:text-foreground/50 **:data-sublink:text-sm **:data-sublink:hidden **:data-sublink:md:block">
+      <ul className="w-full flex flex-col md:flex-row justify-between gap-4 px-8 *:flex *:flex-col *:gap-2 **:data-mainlink:font-bold **:data-mainlink:hover:text-shadow-md **:data-mainlink:hover:text-shadow-foreground/20 **:data-sublink:text-foreground/50 **:data-sublink:text-sm **:data-sublink:hidden **:data-sublink:md:block">
         <li>
           <a data-mainlink href="#">
             home
